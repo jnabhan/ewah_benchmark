073c4938be4f45de8c7ac1c5cf54544f1c1ea46c
Daniel Lemire <lemire@gmail.com>
2015-10-13 12:19:47 -0400

    Adding OR benchmarks.

--- benchmark/test/ewah_test.cc
+++ benchmark/test/ewah_test.cc
@@ -19,5 +19,22 @@ static void BM_logicaland(benchmark::State& state) {
 }
 BENCHMARK(BM_logicaland)->DenseRange(0, 207);
 
+static void BM_logicalor(benchmark::State& state) {
+	string filename = to_string(state.range_x() * 1000);
+	EWAHBoolArray<uint64_t>** ewahs = JavaEWAHReader::readFile("../../bitmap_dumps/" +filename);
+	EWAHBoolArray<uint64_t>* container = new EWAHBoolArray<uint64_t>;
+
+	while (state.KeepRunning()) {
+		ewahs[0]->logicalor(*ewahs[1], *container);
+	}
+
+	delete container;
+	delete ewahs[0];
+	delete ewahs[1];
+	delete[] ewahs;
+}
+
+BENCHMARK(BM_logicalor)->DenseRange(0, 207);
+
 
 BENCHMARK_MAIN()
